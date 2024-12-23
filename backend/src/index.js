fdd115761457beb7504c3e0dc5ca3038b6bc9fbc
pawlurking <pawlurking@gmail.com>
2024-12-23 19:40:40 +0700

    update SignUpPage & LoginPage"

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,7 @@ import {connectDB} from './lib/db.js';
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 dotenv.config()
 
@@ -16,6 +17,12 @@ const portNumber = process.env.portNumber;
 app.use(express.json());
 // This allows us to parse the cookies
 app.use(cookieParser());
+app.use(cors(
+  {
+    origin: "http://localhost:5173",
+    credentials: true,
+  }
+))
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
