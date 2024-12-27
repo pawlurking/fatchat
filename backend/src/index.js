cc3a7b0a04763ead715676aff62a127f235e17a7
pawlurking <pawlurking@gmail.com>
2024-12-27 12:48:36 +0700

    update prod config

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,12 +7,14 @@ import messageRoutes from './routes/message.route.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import {app, server} from "./lib/socket.js";
+import path from "path";
+
 
 dotenv.config()
 
 // const app = express();
 const portNumber = process.env.portNumber;
-
+const __dirname = path.resolve();
 
 // This allow us to parse JSON bodies
 app.use(express.json());
@@ -27,6 +29,16 @@ app.use(cors(
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+// if we currently in production, use dist of frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  // if we visit any routes than "/api/auth" or "/api/messages", we would like to see entry point of our react application
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  })
+}
 
 server.listen(portNumber, () => {
   console.log(`Server is running on port ${portNumber}`);
